481798ad5ce04353327f001c56824b5b9d3a17ff
abhiroop43 <abhiroop.santra@gmail.com>
2016-03-18 13:01:00 +0400

    First check in
    First check in

---
+++ AzureUI/js/controllers/LoginController.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+MetronicApp.controller("LoginController", [
+    "$log", "LoginService", function ($log, loginService) {
+        var login = this;
+        login.date = new Date();
+        $log.log("Inside login controller");
+
+        var doLogin = function(username, password) {
+
+        };
+    }
+]);
---
+++ AzureUI/js/services/LoginService.js
@@ -0,0 +1,6 @@
+﻿"use strict";
+
+MetronicApp.factory("loginService", function (Restangular) {
+    console.log('Calling Login service API');
+    return {};
+});
